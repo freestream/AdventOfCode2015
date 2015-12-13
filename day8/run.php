6848bf0f0f71a64e8d4144ba4e713a8fa7a79ccd
Anton Samuelsson <samuelsson.anton@gmail.com>
2015-12-13 18:20:49 +0100

    Added day 8

---
+++ day8/run.php
@@ -0,0 +1,13 @@
+<?php
+$fp     = fopen('input', 'r');
+$first  = 0;
+$second = 0;
+
+foreach (file('input', FILE_IGNORE_NEW_LINES) as $line) {
+    eval('$str = ' . $line . ';');
+    $first += strlen($line) - strlen($str);
+    $second += strlen(addslashes($line))+2-strlen($line);
+}
+
+echo "In the first run it was {$first} number of characters of code and in the seocnd it was {$second}.";
+
